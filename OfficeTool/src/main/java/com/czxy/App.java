531137fdf7a6bf256c862db00e11c625862818f7
lei <3171494605@qq.com>
2020-05-19 13:22:24 +0800

    scheduled

--- OfficeTool/src/main/java/com/czxy/App.java
+++ OfficeTool/src/main/java/com/czxy/App.java
@@ -3,6 +3,7 @@ package com.czxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,7 @@ import com.czxy.interceptor.IsLoginInterceptor;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class App implements WebMvcConfigurer {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
