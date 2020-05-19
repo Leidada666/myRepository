5ddd71d39004127d4d172803ff52e4e273debba2
lei <3171494605@qq.com>
2020-05-19 13:22:54 +0800

    scheduled complete

---
+++ OfficeTool/src/main/java/com/czxy/util/ScheduledUtil.java
@@ -0,0 +1,51 @@
+package com.czxy.util;
+
+import java.io.File;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.czxy.filepath.FilepathEnum;
+
+@Component
+public class ScheduledUtil {
+	
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+	
+	private final static String filepath = FilepathEnum.EmergeFilePath.getPath();
+	
+	private File dir;
+	
+	private Set<String> keys;
+	
+	@Scheduled(cron="0 0 24 * * ?")
+	public void scheduledMethod() {
+		System.out.println("触发器被执行");
+		keys = this.redisTemplate.keys("*");
+		if(keys.isEmpty() || keys.size() == 0) {
+			System.out.println("缓存为空");
+		}else {
+			for(String s : keys) {
+				System.out.println(keys);
+				this.redisTemplate.delete(s);
+			}
+		}
+		
+		dir = new File(filepath);
+		if(!dir.exists()) {
+			System.out.println("文件夹不存在");
+		}else {
+			if(dir.isDirectory()) {
+				for(File file : dir.listFiles()) {
+					if(!file.getName().equals("ls521")) {
+						file.delete();
+					}
+				}
+			}
+		}
+	}
+}
